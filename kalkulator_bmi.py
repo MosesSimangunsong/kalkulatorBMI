b5a68c9260a20a6f605a261cb1916c92c8278d2a
MosesSimangunsong <simangunsongmoses21@gmail.com>
2025-05-31 12:44:03 +0700

    feat: tambah program perhitungan BMI berdasarkan input pengguna

---
+++ kalkulator_bmi.py
@@ -0,0 +1,28 @@
+#PROGRAM PERHITUNGAN BMI
+
+print ("Perhitungan BMI (Body Mass Index)")
+print ("---------------------------------")
+
+berat_badan = input ("Masukkan berat badan Anda (Kilogram): ")
+berat_badan = float (berat_badan)
+
+tinggi_badan = input ("Masukkan tinggi badan Anda (Meter):")
+tinggi_badan = float (tinggi_badan)
+
+print (berat_badan)
+print (tinggi_badan)
+
+bmi = ((berat_badan)/ (tinggi_badan**2))
+
+berat_badan_ideal = dict()
+berat_badan_ideal['bawah'] = 18.5 * (tinggi_badan**2)
+berat_badan_ideal['atas'] = 25 * (tinggi_badan**2)
+
+
+
+print(f"Nilai BMI Anda adalah{bmi : .2f} kg/m^2")
+print("Nilai BMI normal adalah 18.5 kg/m^2 - 25 kg/m^2")
+
+print (f"Berat badan ideal untuk anda adalah dalam rentang {berat_badan_ideal['bawah']:.2f} kg - {berat_badan_ideal['atas']:.2f} kg")
+
+print ("Terimakasih sudah menggunakan program ini :)")
